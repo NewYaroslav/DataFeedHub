6e4331709d541cdbadd45ec0803b5bacb9aefaa8
Pureloglow <strelnikov-sv@yandex.ru>
2025-12-16 10:22:58 +0300

    v.1.2.0
    fix(utils): использовать gzip::Decompressor для извлечения GZIP

--- include/DataFeedHub/utils/zip_utils.hpp
+++ include/DataFeedHub/utils/zip_utils.hpp
@@ -126,7 +126,10 @@ namespace dfh::utils {
     /// \throws std::runtime_error If input is empty or decompression fails.
     inline std::string extract_from_gzip(const std::string& gzip_data, size_t max_bytes = 2000000000) {
         if (gzip_data.empty()) throw std::runtime_error("GZIP archive is empty.");
-        return gzip::decompress(gzip_data, max_bytes);
+        gzip::Decompressor decompressor(max_bytes);
+        std::string decompressed;
+        decompressor.decompress(decompressed, gzip_data.data(), gzip_data.size());
+        return decompressed;
     }
 
 }; // namespace dfh::utils
